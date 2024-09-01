370a936f8bcd5468d39366e0bb1600e6e6ff7a3b
Kush Chaudhari <ckushj@gmail.com>
2024-09-01 20:16:11 +0530

    improved ui of home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import Provider from "./Provider"
 import Router from "./Router"
 import Container from "./Container"
 import "./styles/styles.css"
+import "./styles/home.css"
 // import "./styles/Canva-Clone/css/style.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    '/',
+    './**/*.{js,jsx,ts,tsx}', // Adjust these paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
